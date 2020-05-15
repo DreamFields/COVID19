4d77593e6ef02d1345d8aceeb603fd5520f77f15
motohbq <1339611623@qq.com>
2020-05-15 11:34:42 +0800

    Updates

---
+++ precache-manifest.c0535000f538f5199dc188cb16b8086f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "186d8e6b9aec0b20f3e40c7950f9dcee",
+    "url": "/COVID19/index.html"
+  },
+  {
+    "revision": "8a0ae7362f1d0dce29e7",
+    "url": "/COVID19/static/js/2.fde69909.chunk.js"
+  },
+  {
+    "revision": "897bb2fefb07d4775fdc2c464d6a5bfd",
+    "url": "/COVID19/static/js/2.fde69909.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93480c5f978d621daaff",
+    "url": "/COVID19/static/js/main.23ece0b2.chunk.js"
+  },
+  {
+    "revision": "6d4a01914add109d4fd7",
+    "url": "/COVID19/static/js/runtime-main.d2a9c459.js"
+  },
+  {
+    "revision": "da2cdef79cb3e2a848d77fd55cda077b",
+    "url": "/COVID19/static/media/bg.da2cdef7.png"
+  },
+  {
+    "revision": "40c2442d1a531ae596a3ba6ed2cb395a",
+    "url": "/COVID19/static/media/header_bg.40c2442d.png"
+  }
+]);
